458e2e4363bdc976029f0c32c654471eb190c231
1989Ryan <864259222@qq.com>
2019-04-22 11:04:39 +0800

    bug in run.sh fixed

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 gnome-terminal -x bash -c "roscore"
-sleep 3
+sleep 2
 gnome-terminal -x bash -c "cd Third_Part/PSPNet_Keras_tensorflow/
-python Semantic_Information_Publisher.py image:=/read_image"
+python Semantic_Information_Publisher.py image:=/camera/image_raw"
 sleep 1
 gnome-terminal -x bash -c "cd src/
-python map_engine.py catorymap:=/cm KeyPoints:=/KeyPoint MapPoints:=/mappoint"
+python map_engine.py"
 sleep 1
-gnome-terminal -x bash -c "cd Third_Part/ORBSLAM2/ 
-rosrun ORB_SLAM2 Mono Vocabulary/ORBvoc.txt Examples/ROS/ORB_SLAM2/Asus.yaml /camera/image_raw:=/result"
-sleep 1
+gnome-terminal -x bash -c " cd Third_Part/ORB_SLAM2/
+rosrun ORB_SLAM2 Mono Vocabulary/ORBvoc.txt Examples/Monocular/KITTI00-02.yaml"
 gnome-terminal -x bash -c "cd src/
 python cluster.py"
