870d8a7bff774ae64195d4f5eb498148636124dc
1989Ryan <864259222@qq.com>
2019-04-22 11:01:27 +0800

    fixed bugs in run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,8 @@ sleep 1
 gnome-terminal -x bash -c "cd src/
 python map_engine.py catorymap:=/cm KeyPoints:=/KeyPoint MapPoints:=/mappoint"
 sleep 1
-gnome-terminal -x bash -c " rosrun ORB_SLAM2 Mono Vocabulary/ORBvoc.txt Examples/ROS/ORB_SLAM2/Asus.yaml /camera/image_raw:=/result"
+gnome-terminal -x bash -c "cd Third_Part/ORBSLAM2/ 
+rosrun ORB_SLAM2 Mono Vocabulary/ORBvoc.txt Examples/ROS/ORB_SLAM2/Asus.yaml /camera/image_raw:=/result"
 sleep 1
 gnome-terminal -x bash -c "cd src/
 python cluster.py"
